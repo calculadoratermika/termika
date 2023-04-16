6c238080b0ebc3befa9c0003e71dbd17011ab5a7
Guille <gtiscornia@outlook.com>
2023-04-16 15:46:02 -0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,1 +1,12 @@
-// borrar
+
+function getRandomNumberBetween(min,max){
+  return Math.floor(Math.random()*(max-min+1)+min)
+}
+
+let ruleta
+ruleta = getRandomNumberBetween(1,27)
+
+const ruleta_fija = ruleta
+
+console.log(ruleta)
+console.log(ruleta_fija)
